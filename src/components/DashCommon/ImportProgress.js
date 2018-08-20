eda428479fd08a112e75b9c44389db252857befa
capmouaad <mouaadhama1@gmail.com>
2018-08-20 15:27:29 -0700

    Data Import Prgress pooling & Timer Unmount

--- src/components/DashCommon/ImportProgress.js
+++ src/components/DashCommon/ImportProgress.js
@@ -4,113 +4,113 @@ import PropTypes from 'prop-types';
 import api from '../../services/Api';
 
 class ImportProgress extends Component {
-  static propTypes = {
-    signupId: PropTypes.number
-  };
+    static propTypes = {
+        signupId: PropTypes.number
+    };
 
-  constructor(props){
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      FinanceDataImportProgress: 0,
-      ReportDataImportProgress: 0,
-      AdDataImportProgress: 0
-    }
-  }
-
-  componentDidMount(){
-    this.getImportStatus();
-
-    // this pull data
-    // let checkAPI = setInterval( () => {
-    //   if ( this.state.FinanceDataImportProgress !== 100 &&
-    //        this.state.ReportDataImportProgress !== 100 &&
-    //        this.state.AdDataImportProgress !== 100
-    //   ){
-    //     this.getImportStatus();
-    //   } else {
-    //     clearInterval(checkAPI)
-    //   }
-    // }, 1000) // ajax pooling
-
-  }
-
-  getImportStatus = () => {
-
-    const { signupId } = this.props; // prodcution
-
-    api
-      .get(`GetDataImportStatus?clientid=${signupId}`)
-      .then((res) => {
-        console.log('backend responce to GET GetDataImportStatus', res)
-
-        if ( res.data.IsSuccess ){
-
-          this.setState({
-            FinanceDataImportProgress: res.data.FinanceDataImportProgress,
-            ReportDataImportProgress: res.data.ReportDataImportProgress,
-            AdDataImportProgress: res.data.AdDataImportProgress
-          })
-        } else {
-
+        this.state = {
+            FinanceDataImportProgress: 0,
+            ReportDataImportProgress: 0,
+            AdDataImportProgress: 0,
+            DataImportComplete: false
         }
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }
+    }
 
-  render(){
+    componentDidMount() {
+        this.getImportStatus();
 
-    const { FinanceDataImportProgress, ReportDataImportProgress, AdDataImportProgress } = this.state
+        this.timerGetImportStatus = setInterval(() => {
+            if (!this.state.DataImportComplete) {
+                this.getImportStatus();
+            } else {
+                clearInterval(this.timerGetImportStatus);
+            }
+        }, 5000);
+    }
 
-    return(
-      <div className="i-progress">
-        <div className="container container--full">
-          <div className="i-progress__wrapper">
-            <div className="i-progress__title">DATA IMPORT STATUS</div>
-            <div className="i-progress__bars">
-              <IBar name="Financials" progress={FinanceDataImportProgress} />
-              <IBar name="Products" progress={ReportDataImportProgress} />
-              <IBar name="Advertising" progress={AdDataImportProgress} />
+    componentWillUnmount() {
+        clearInterval(this.timerGetImportStatus);
+    }
+
+    getImportStatus = () => {
+
+        const { signupId } = this.props; // prodcution
+
+        api
+            .get(`GetDataImportStatus?clientid=${signupId}`)
+            .then((res) => {
+                console.log('backend responce to GET GetDataImportStatus', res)
+
+                if (res.data.IsSuccess) {
+                    this.setState({
+                        FinanceDataImportProgress: res.data.FinanceDataImportProgress,
+                        ReportDataImportProgress: res.data.ReportDataImportProgress,
+                        AdDataImportProgress: res.data.AdDataImportProgress,
+                        DataImportComplete: res.data.DataImportComplete
+                    })
+                } else {
+
+                }
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
+    }
+
+    render() {
+
+        const { FinanceDataImportProgress, ReportDataImportProgress, AdDataImportProgress } = this.state
+
+        return (
+            <div className="i-progress">
+                <div className="container container--full">
+                    <div className="i-progress__wrapper">
+                        <div className="i-progress__title">DATA IMPORT STATUS</div>
+                        <div className="i-progress__bars">
+                            <IBar name="Financials" progress={FinanceDataImportProgress} />
+                            <IBar name="Products" progress={ReportDataImportProgress} />
+                            <IBar name="Advertising" progress={AdDataImportProgress} />
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 
 const IBar = (props) => {
-  const { name, progress } = props
-  const isCompleated = progress === 100
+    const { name, progress } = props
+    const isCompleated = progress === 100
 
-  return(
-    <div className="i-bar">
-      <div className="i-bar__name">{name}</div>
-      <div className="i-bar__status">
-        <div
-          className={"i-bar__progress" + (isCompleated ? " is-compleated" : "")}
-          style={{"width" : `${progress}%`}}
-        >
-          {
-            progress > 5 &&
-            <span>{isCompleated ? "Complete" : progress + " %" }</span>
-          }
+    return (
+        <div className="i-bar">
+            <div className="i-bar__name">{name}</div>
+            <div className="i-bar__status">
+                <div
+                    className={"i-bar__progress" + (isCompleated ? " is-compleated" : "")}
+                    style={{ "width": `${progress}%` }}
+                >
+                    {
+                        progress > 5 &&
+                        <span>{isCompleated ? "Complete" : progress + " %"}</span>
+                    }
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 
 const mapStateToProps = (state) => ({
-  signupId: state.signup.signupId
+    signupId: state.signup.signupId
 });
 
 const mapDispatchToProps = (dispatch) => ({
-  // setSignupFields: (data) => dispatch(setSignupFields(data))
+    // setSignupFields: (data) => dispatch(setSignupFields(data))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(ImportProgress);
