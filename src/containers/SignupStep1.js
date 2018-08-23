606e92a05a9ee45bcc7020f63d256efa5fb14625
capmouaad <mouaadhama1@gmail.com>
2018-08-23 12:00:25 -0700

    Indentation and commented code removal

--- src/containers/SignupStep1.js
+++ src/containers/SignupStep1.js
@@ -6,38 +6,13 @@ import ReCAPTCHA from 'react-google-recaptcha';
 import { withRouter } from 'react-router-dom'
 
 import { setSignupStep, setSignupEmail, setSignupId, setSignupFields } from '../actions/signup';
-import {  setAuthToken  } from '../actions/login';
+import { setAuthToken } from '../actions/login';
 import api from '../services/Api';
 import FormInput from '../components/Forms/FormInput';
 import PassMeter from '../components/Forms/PassMeter';
 import FormLoader from '../components/Forms/FormLoader';
 import { tAndC, privacyPolicy } from '../components/Footer';
-import Toaster, {showToastMessage} from '../services/toasterNotification'
-
-// Formsy.addValidationRule('isValidPassword', (values, value) => {
-//   // const min_length = value ? value.length >= 8 : false
-//   // const contains_upcase = /[A-Z]/.test(value)
-//   // const contains_lowercase = /[a-z]/.test(value)
-//   // const contains_digits = /\d/.test(value)
-//   //
-//   // const rules = {
-//   //   min_length: min_length,
-//   //   contains_upcase: contains_upcase,
-//   //   contains_lowercase: contains_lowercase,
-//   //   contains_digits: contains_digits
-//   // }
-//   //
-//   // const valiations = Object.values(rules)
-//   // let counter = 0;
-//   // let name = ""
-//   // valiations.forEach( (x) => x === true ? counter++ : false )
-//   //
-//   // if ( counter >= 3 ){
-//   //   return true
-//   // } else {
-//   //   return false
-//   // }
-// });
+import Toaster, { showToastMessage } from '../services/toasterNotification'
 
 class SignupStep1 extends Component {
   static propTypes = {
@@ -57,8 +32,8 @@ class SignupStep1 extends Component {
 
     this.state = {
       first_name: props.signupFields.first_name,
-      last_name:  props.signupFields.last_name,
-      company_name:  props.signupFields.company_name,
+      last_name: props.signupFields.last_name,
+      company_name: props.signupFields.company_name,
       email: props.signupEmail,
       phone: props.signupFields.phone,
       password: props.signupFields.password,
@@ -72,7 +47,6 @@ class SignupStep1 extends Component {
 
     this.formRef = React.createRef();
   }
-
 
   formInvalid = () => {
     this.setState({ formIsValid: false });
@@ -93,22 +67,22 @@ class SignupStep1 extends Component {
     if (
       this.state.formIsValid &&
       this.state.captcha &&
-      ( this.state.password === this.state.password_confirmation )
-    ){
+      (this.state.password === this.state.password_confirmation)
+    ) {
       this.nextStep();
     }
   }
 
   // submit handler for the form (prevalidations, etc)
   submitForm = () => {
-    if ( this.state.password === this.state.password_confirmation ){
-      this.setState({passwords_match: true})
+    if (this.state.password === this.state.password_confirmation) {
+      this.setState({ passwords_match: true })
     } else {
-      this.setState({passwords_match: false})
+      this.setState({ passwords_match: false })
     }
 
-    if ( this.state.captcha === null ){
-      this.setState({captcha: false})
+    if (this.state.captcha === null) {
+      this.setState({ captcha: false })
     }
 
     this.setState({
@@ -119,7 +93,7 @@ class SignupStep1 extends Component {
   handleChange = (e) => {
     let fieldName = e.target.name;
     let fleldVal = e.target.value;
-    this.setState({...this.state, [fieldName]: fleldVal});
+    this.setState({ ...this.state, [fieldName]: fleldVal });
   }
 
   nextStep = () => {
@@ -144,12 +118,12 @@ class SignupStep1 extends Component {
       .get(`CheckEmail?Email=${email}`)
       .then((res) => {
         console.log('backend responce to Get CheckEmail', res)
-        if ( res.data.IsDuplicateUser ){
-          history.push(`/login`)         
+        if (res.data.IsDuplicateUser) {
+          history.push(`/login`)
           this.formRef.current.updateInputsWithError({
             email: res.data.ErrorMessage
           });
-          this.setState({isFormSubmited: false})
+          this.setState({ isFormSubmited: false })
         } else {
           this.createUser(leadObj) // move on if it's fine
         }
@@ -165,8 +139,7 @@ class SignupStep1 extends Component {
       .post(`Signup`, leadObj)
       .then((res) => {
         console.log('back-end responce to post Signup', res)
-        if ( res.data.IsSuccess ){
-          // this.props.setSignupId(res.data.id); // TODO
+        if (res.data.IsSuccess) {
           this.props.setSignupEmail(res.config.data.email);
           this.props.setSignupId(res.data.UserInfo.ClientID);
           this.props.setAuthToken(res.data.AuthToken);
@@ -182,7 +155,7 @@ class SignupStep1 extends Component {
           showToastMessage(res.data.ErrorMessage, "Error");
         }
 
-        this.setState({isFormSubmited: false})
+        this.setState({ isFormSubmited: false })
       })
       .catch(function (error) {
         console.log(error);
@@ -215,10 +188,9 @@ class SignupStep1 extends Component {
       .catch(err => {
 
       });
-
   }
 
-  async updateStepOnBackend(){
+  async updateStepOnBackend() {
     const res = await api.post('UpdateCurrentStepAsync?step=ConnectSellerCentral');
     return await res.data;
   }
@@ -236,17 +208,16 @@ class SignupStep1 extends Component {
     }
   }
 
-  componentDidMount () {
+  componentDidMount() {
     this.getEmailUrl()
   }
 
-
-  render(){
+  render() {
     const { first_name, last_name, company_name, email, phone, password, password_confirmation, apiError, isFormSubmited } = this.state;
 
-    return(
+    return (
       <div className="signup__container">
-     <Toaster/>
+        <Toaster />
         <Formsy
           className="signup__form"
           onSubmit={this.submitForm}
@@ -255,9 +226,9 @@ class SignupStep1 extends Component {
           onInvalid={this.formInvalid}
           ref={this.formRef}
         >
-          <div className={"loader-container " + (isFormSubmited ? "is-loading" : null) }>
+          <div className={"loader-container " + (isFormSubmited ? "is-loading" : null)}>
             <FormLoader />
-            { apiError &&
+            {apiError &&
               <span className="ui-input-validation">{apiError}</span>
             }
             <FormInput
@@ -287,16 +258,16 @@ class SignupStep1 extends Component {
               required
             />
             <FormInput
-                name="company_name"
-                label="Company Name"
-                placeholder="Sales Warehouse LLC"
-                value={company_name}
-                onChangeHandler={this.handleChange}
-                validationErrors={{
-                  isDefaultRequiredValue: 'Please enter company name'
-                }}
-                required
-              />
+              name="company_name"
+              label="Company Name"
+              placeholder="Sales Warehouse LLC"
+              value={company_name}
+              onChangeHandler={this.handleChange}
+              validationErrors={{
+                isDefaultRequiredValue: 'Please enter company name'
+              }}
+              required
+            />
             <FormInput
               name="email"
               label="Email Address"
@@ -349,7 +320,7 @@ class SignupStep1 extends Component {
               onChangeHandler={this.handleChange}
               required
             />
-            { this.state.passwords_match === false &&
+            {this.state.passwords_match === false &&
               <span className="ui-input-validation">Passwords did not match</span>
             }
             <div className="signup__captcha">
@@ -360,11 +331,11 @@ class SignupStep1 extends Component {
                 onChange={this.recaptchaVerify}
               />
             </div>
-            { this.state.captcha === false &&
+            {this.state.captcha === false &&
               <span className="ui-input-validation">Please enter captcha</span>
             }
             <div className="signup__rules">
-                        By signing up, you are agreeing to KiniMetrix’s <br /><a onClick={tAndC} rel="noopener noreferrer">Terms of Use</a> and <a onClick={privacyPolicy} rel="noopener noreferrer">Privacy Policy</a>.
+              By signing up, you are agreeing to KiniMetrix’s <br /><a onClick={tAndC} rel="noopener noreferrer">Terms of Use</a> and <a onClick={privacyPolicy} rel="noopener noreferrer">Privacy Policy</a>.
             </div>
             <div className="signup__form-cta">
               <button type="submit" className="btn btn-signup btn--block">Sign Up</button>
@@ -375,7 +346,6 @@ class SignupStep1 extends Component {
     )
   }
 }
-
 
 const mapStateToProps = (state) => ({
   signupFields: state.signup.fields,
