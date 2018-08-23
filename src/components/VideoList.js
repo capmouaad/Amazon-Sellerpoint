94e18b9820134ddab111c851aeb262ba5b67f166
capmouaad <mouaadhama1@gmail.com>
2018-08-23 15:06:31 -0700

    Indentation

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -3,40 +3,45 @@ import YouTube from 'react-youtube';
 import api from '../services/Api';
 
 export default class VideoList extends Component {
-  render(){
+  render() {
     const videoList = [
       {
         name: "NAVIGATION",
         desc: "Learn how to move around the app.",
         videoIdYouTube: "kcLIqGuFHDg"
-      },{
+      },
+      {
         name: "SETTINGS",
         desc: "How to setup product costs information and create SKU groups.",
         videoIdYouTube: "bAmn4eS5-10"
-      },{
+      },
+      {
         name: "CHARTS",
         desc: "Control your chart views.",
         videoIdYouTube: "pY-3qZIXq34"
-      },{
+      },
+      {
         name: "FINANCIAL DASHBOARD",
         desc: "Understand net and gross profit and how to read trend charts.",
         videoIdYouTube: "oK3ebrIiFFc"
-      },{
+      },
+      {
         name: "BUSINESS RESULTS DASHBOARD",
         desc: "Learn how to quickly scan revenue, sales quantity, pricing and profit margin.",
         videoIdYouTube: "_nk0VGcM5lc"
-      },{
+      },
+      {
         name: "OPERATIONS DASHBOARD",
         desc: "Understand how key performance metrics can save you time in managing the business.",
         videoIdYouTube: "xrpdlKM3yH8"
       }
     ]
 
-    return(
+    return (
       <div className="video-list">
         <h2 className="video-list__title">Quick introductions to get you started</h2>
         <div className="video-list__grid">
-          { videoList.map( (video, i) => {
+          {videoList.map((video, i) => {
             return (
               <VideoEl
                 key={i}
@@ -44,7 +49,7 @@ export default class VideoList extends Component {
                 desc={video.desc}
                 videoIdYouTube={video.videoIdYouTube} />
             )
-          }) }
+          })}
         </div>
       </div>
     )
@@ -54,7 +59,7 @@ export default class VideoList extends Component {
 const VideoEl = (props) => {
   const { name, desc, videoIdYouTube } = props
 
-  const _onPlay = (event) =>{
+  const _onPlay = (event) => {
     api
       .post(`VideoWatched`)
       .then((res) => {
@@ -65,7 +70,7 @@ const VideoEl = (props) => {
       });
   }
 
-  return(
+  return (
     <div className="video-list__col">
       <div className="video-element">
         <div className="video-element__scaler">
