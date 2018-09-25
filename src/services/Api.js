7c76930418d4b6f58ef17f7fb1020570dc85babe
capmouaad <mouaadhama1@gmail.com>
2018-09-25 21:49:29 +0530

    Api url changed from local to QA

--- src/services/Api.js
+++ src/services/Api.js
@@ -5,11 +5,11 @@ import { resetStateDashFilter } from '../actions/dashFilter'
 import { closeAppQlik } from '../actions/qlik'
 import { resetStatusBar } from '../actions/statusBar'
 import { setShowImportProgressBar } from '../actions/header'
-import { RESET_STATE_SIGNUP, SET_STATUS_PROGRESS, SET_NAVBAR_DASHBOARD} from '../store/ActionTypes'
+import { RESET_STATE_SIGNUP, SET_STATUS_PROGRESS, SET_NAVBAR_DASHBOARD } from '../store/ActionTypes'
 
 //const BACKEND_URL = process.env.NODE_ENV === 'production' ? "http://name.herokuapp.com" : "http://localhost:8000/"
- const BACKEND_URL = "http://localhost:10547/api/SellerPoint/"
-//const BACKEND_URL = "https://qa.kinimetrix.com/api/SellerPoint/";
+//const BACKEND_URL = "http://localhost:10547/api/SellerPoint/"
+const BACKEND_URL = "https://qa.kinimetrix.com/api/SellerPoint/";
 //const BACKEND_URL = window.location.origin + '/api/SellerPoint/';
 
 const api = axios.create({
