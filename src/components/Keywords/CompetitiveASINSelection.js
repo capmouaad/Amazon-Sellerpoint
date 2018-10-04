b8f48329939187e9fedec28555a4003933a292a2
capmouaad <mouaadhama1@gmail.com>
2018-10-04 18:47:21 +1000

    fixed 1099

--- src/components/Keywords/CompetitiveASINSelection.js
+++ src/components/Keywords/CompetitiveASINSelection.js
@@ -3,94 +3,6 @@ import api from '../../services/Api'
 import FormLoader from './../Forms/FormLoader'
 import React from 'react'
 
-// Dummy data
-const res = {
-  "MatchingProducts": [
-    {
-        "ASIN": "B01BY9RTNQ",
-        "Brand": "PowerLix",
-        "Title": "PowerLix Milk Frother Handheld Battery Operated Electric Foam Maker For Coffee, Latte, Cappuccino, Hot Chocolate, Durable Drink Mixer With Stainless Steel Whisk, Stainless Steel Stand Include",
-        "SalesCategory": "14042381",
-        "SalesRank": 1,
-        "ListPrice": ""
-    },
-    {
-        "ASIN": "B06ZXXNMNS",
-        "Brand": "VIVREAL",
-        "Title": "Milk Frother - Milk Steamer Electric Frother for Soft Foam, Automatic Milk Frother Hot Cold Milk Frother and Warmer with Two Whisks, Frother for Coffee, Creamer Frother Milk Heater 4.25 oz/8.5 oz",
-        "SalesCategory": "14042381",
-        "SalesRank": 4,
-        "ListPrice": "89.99"
-    },
-    {
-        "ASIN": "B072W1MWDG",
-        "Brand": "Chef's Star",
-        "Title": "Chef's Star MF-2 Premier Automatic Milk Frother, Heater and Cappuccino Maker with New Foam Density Feature (New Version)",
-        "SalesCategory": "14042381",
-        "SalesRank": 8,
-        "ListPrice": "109.99"
-    },
-    {
-        "ASIN": "B076F3C4XP",
-        "Brand": "Bonsenkitchen",
-        "Title": "Bonsenkitchen Electric Handheld Milk Frother for Making Lattes, Cappuccinos and Hot Chocolates, Battery Included, Stainless Steel Shaft and Whip (MF8710)",
-        "SalesCategory": "14042381",
-        "SalesRank": 3,
-        "ListPrice": "15.99"
-    },
-    {
-        "ASIN": "B07D2Z6QDL",
-        "Brand": "CHINYA",
-        "Title": "Milk Frother, Automatic Milk Steamer with New Foam Density Feature, Electric Frother with Hot or Cold Milk Function for Coffee, Cappuccino and Breakfast (Silver)",
-        "SalesCategory": "14042381",
-        "SalesRank": 10,
-        "ListPrice": ""
-    },
-    {
-        "ASIN": "B07CJJLKN6",
-        "Brand": "ISILER",
-        "Title": "Milk Frother, iSiLER Electric Milk Frother, 130ml(4.5OZ) Automatic Hot Cold Milk Frother, 300ml(10.5OZ) Milk Heater with Non-Stick Coating Copper Thermostat for Coffee, Hot Chocolate, Creamer",
-        "SalesCategory": "14042381",
-        "SalesRank": 13,
-        "ListPrice": ""
-    },
-    {
-        "ASIN": "B06XHWQJKN",
-        "Brand": "Nespresso",
-        "Title": "Nespresso 3694-US-BK Aeroccino3 Milk Frother, One Size, Black",
-        "SalesCategory": "14042381",
-        "SalesRank": 11,
-        "ListPrice": "99.00"
-    },
-    {
-        "ASIN": "B01IYDNTAW",
-        "Brand": "Elementi",
-        "Title": "Milk Frother with Stand (Black) - Make Cappuccinos, Lattes and Bulletproof Coffee - Handheld with More Powerful High Torque Motor",
-        "SalesCategory": "14042381",
-        "SalesRank": 6,
-        "ListPrice": "17.99"
-    },
-    {
-        "ASIN": "B00SKFCSB6",
-        "Brand": "1Easylife",
-        "Title": "1Easylife H422 Stainless Steel Handheld Electric Milk Frother with Bonus Mix Spoon",
-        "SalesCategory": "14042381",
-        "SalesRank": 9,
-        "ListPrice": "59.99"
-    },
-    {
-        "ASIN": "B003LXY2HA",
-        "Brand": "Capresso",
-        "Title": "Capresso 202.04 frothPRO Automatic Milk Frother and Hot Chocolate Maker",
-        "SalesCategory": "14042381",
-        "SalesRank": 15,
-        "ListPrice": "75.00"
-    }
-],
-"IsSuccess": true,
-"ErrorMessage": null
-}
-
 export default class CompetitiveASINSelection extends React.PureComponent {
   constructor (props) {
     super (props)
@@ -107,26 +19,28 @@ export default class CompetitiveASINSelection extends React.PureComponent {
   }
 
   onGetCompetitorASIN = async () => {
-    // Dummy Data
-    this.setState({
-      MatchingProducts: res.MatchingProducts
-    })
-    // try {
-    //   const { location } = this.props
-    //   const params = {
-    //     keywords: location.state ? location.state.inputKeywords: null,
-    //     marketplaceId: location.state ? location.state.marketplaceId : null,
-    //     InputASIN: location.state ? location.state.asin : null
-    //   }
+    try {
+      this.setState({
+        isGetDataBE: true
+      })
+      const { location } = this.props
+      const params = {
+        keywords: location.state ? location.state.inputKeywords: null,
+        marketplaceId: location.state ? location.state.marketplaceId : null,
+        InputASIN: location.state ? location.state.asin : null
+      }
 
-    //   const { data } = await api.post('KWGetCompetitorASIN', params)
-
-    //   this.setState({
-    //     MatchingProducts: data.MatchingProducts
-    //   })
-    // } catch (error) {
-    //   console.log('Error ', error)
-    // }
+      const { data } = await api.post('KWCompetitorASIN', params)
+      this.setState({
+        MatchingProducts: data.MatchingProducts,
+        isGetDataBE: false
+      })
+    } catch (error) {
+      console.log('Error ', error)
+      this.setState({
+        isGetDataBE: false
+      })
+    }
   }
 
   onAddManualAsin = () => {
